006599e4c0493c481478c592cd654e82e4694538
wajihanibnathrafihasnatul-star <wajihanibnathrafihasnatul@gmail.com>
2026-08-21 17:08:42 +0600

    Update add.php

--- add.php
+++ add.php
@@ -3,32 +3,40 @@ include 'db.php';
 
 if(isset($_POST['save'])){
 
-$name=$_POST['name'];
-$quantity=$_POST['quantity'];
-$price=$_POST['price'];
+    $name = $_POST['name'];
+    $quantity = $_POST['quantity'];
+    $price = $_POST['price'];
 
-mysqli_query($conn,
-"INSERT INTO products(product_name,quantity,price)
-VALUES('$name','$quantity','$price')");
+    $stmt = mysqli_prepare(
+        $conn,
+        "INSERT INTO products(product_name, quantity, price) VALUES (?, ?, ?)"
+    );
 
-header("Location:index.php");
+    mysqli_stmt_bind_param($stmt, "sid", $name, $quantity, $price);
+
+    mysqli_stmt_execute($stmt);
+
+    mysqli_stmt_close($stmt);
+
+    header("Location: index.php");
+    exit();
 }
 ?>
 
 <form method="post">
 
 Product Name:
-<input type="text" name="name">
+<input type="text" name="name" required>
 
 <br>
 
 Quantity:
-<input type="number" name="quantity">
+<input type="number" name="quantity" required>
 
 <br>
 
 Price:
-<input type="number" name="price">
+<input type="number" name="price" required>
 
 <br>
 
